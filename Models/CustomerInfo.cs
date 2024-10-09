09c80ecf4a5468983df2f7468532eb395536a13d
Roope Forstén <124146815+torpeforsten@users.noreply.github.com>
2024-10-09 15:21:38 +0300

    Add files via upload

---
+++ Models/CustomerInfo.cs
@@ -0,0 +1,10 @@
+namespace EFApp.Models;
+
+public class CustomerInfo
+{
+    public int CustomerId { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string AddressLine1 { get; set; }
+    public string City { get; set; }
+}
