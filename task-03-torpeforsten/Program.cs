e3c5db25369da3c6ccca84922c75921d731bc793
Roope Forstén <124146815+torpeforsten@users.noreply.github.com>
2024-10-09 15:25:01 +0300

    Add files via upload

---
+++ task-03-torpeforsten/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Entity Framework!");
+
+//string connectionString = "Server=tcp:sav-mik-devsql.database.windows.net,1433;Initial Catalog=AdventureWorksLT;Persist Security Info=False;User ID=ets730021s;Password=qMA9m7b4ZGfEgWsM;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
